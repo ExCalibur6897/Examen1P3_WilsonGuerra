23b6347a3c03582e733c3e6a4108f46184cfcd98
excal <excal@LAPTOP-ACVGNVK1>
2023-05-19 16:19:49 -0600

    Commit Final

--- Examen1P3_WilsonGuerra/Examen1P3_WilsonGuerra.cpp
+++ Examen1P3_WilsonGuerra/Examen1P3_WilsonGuerra.cpp
@@ -1,3 +1,4 @@
+//Incluir todas las librerias y clases necesarias
 #include <iostream>
 #include <vector>
 #include "Auto.h"
@@ -9,14 +10,42 @@
 
 using namespace std;
 
+//Funcion para calcular los resultados de las carreras
+void carrera(double velMax1, double velMax2, double aceleracion1, double aceleracion2, double distancia) {
+	double ta1 = velMax1 / aceleracion1;
+	double ta2 = velMax2 / aceleracion2;
+	double da1 = 0.5 * aceleracion1 * (ta1 * ta1);
+	double da2 = 0.5 * aceleracion2 * (ta2 * ta2);
+	double tvmax1 = (da1 - distancia) / velMax1;
+	double tvmax2 = (da2 - distancia) / velMax2;
+	double t_total1 = ta1 + tvmax1;
+	double t_total2 = ta2 + tvmax2;
+
+	cout << "\nTiempo total auto 1: " << t_total2 << endl;
+	cout << "\nTiempo total auto 2: " << t_total1 << endl;
+	if (t_total2 < t_total1) {
+		cout << "\nEl auto 1 es el ganador!\n";
+		system("pause");
+	}
+	else {
+		cout << "\nEl auto 2 es el ganador!\n";
+		system("pause");
+	}
+}
+
 int main()
 {
+	//Declaracion de variables
 	int opcion = 0;
 	int ciguenal = 0;
 	int cajadc = 0;
 	int diferencial = 0;
 	int neumatico = 0;
 	int radiador = 0;
+	int ordenar = 0;
+	int auto1 = 0;
+	int auto2 = 0;
+	double distancia = 0;
 	string opcionn = "";
 	char marca[50];
 	char modelo[50];
@@ -73,7 +102,7 @@ int main()
 	cajasDeCambios.push_back(cdcc);
 	cajasDeCambios.push_back(cdccc);
 	while (true) {
-		nfs = true;
+		nfs = true; //Booleano para el menu de need for speed mantenerse en el menu de need for speed.
 		system("cls");
 		cout << "Menu Principal\n1. Need for Speed\n2. Salir del programa";
 		cout << "\nIngrese una opcion: ";
@@ -91,6 +120,7 @@ int main()
 				cout << "Bienvenido a Need for Speed!\n";
 				cout << "1. Agregar Auto\n2. Modificar Auto\n3. Ordenar Autos\n4. Carrera\n5. Salir al menu principal\nIngrese una opcion:\n\n";
 				cin >> opcionn;
+				//Conversion para que el programa no crashee en caso de que se ingresen letras en el menu
 				if (opcionn == "1")
 					opcion = 1;
 				else if (opcionn == "2")
@@ -166,27 +196,180 @@ int main()
 							cin >> ciguenal;
 						}
 						ciguenal--;
-						cout << "Resultados de modificacion\n";
+						cout << "\nResultados de modificacion\n";
 						autos[pos]->velMax += ciguenales[ciguenal]->velMax;
-						autos[pos]->caballosFuerza -= ciguenales[ciguenal]->caballosFuerza;
+						autos[pos]->caballosFuerza += ciguenales[ciguenal]->caballosFuerza;
 						cout << "Marca: " << autos[pos]->marca << "\nModelo: " << autos[pos]->modelo << "\nVelocidad Maxima: " << autos[pos]->velMax << "\nCaballos de fuerza: " << autos[pos]->caballosFuerza << "\nAceleracion: " << autos[pos]->aceleracion << endl;
 						system("pause");
 						break;
 					case 2:
-
+						system("cls");
+						cout << "--Modificar Radiador--\n\n";
+						cout << "Radiadores disponibles:\n";
+						for (int i = 0; i < radiadores.size(); i++) {
+							cout << (i + 1) << ". " << "\nCaballos de Fuerza: " << radiadores[i]->caballosFuerza << endl;
+						}
+						cout << "Seleccione radiador: ";
+						cin >> radiador;
+						while (radiador < 1 || radiador > radiadores.size()) {
+							cout << "Radiador invalido!\nSeleccione radiador: ";
+							cin >> radiador;
+						}
+						radiador--;
+						autos[pos]->caballosFuerza += radiadores[radiador]->caballosFuerza;
+						cout << "\nResultados de modificacion\n";
+						cout << "Marca: " << autos[pos]->marca << "\nModelo: " << autos[pos]->modelo << "\nVelocidad Maxima: " << autos[pos]->velMax << "\nCaballos de fuerza: " << autos[pos]->caballosFuerza << "\nAceleracion: " << autos[pos]->aceleracion << endl;
+						system("pause");
 						break;
-					}
 					case 3:
 						system("cls");
-						cout << "Ordenar Autos\n";
+						cout << "--Modificar Neumaticos--\n\n";
+						cout << "Neumaticos disponibles:\n";
+						for (int i = 0; i < neumaticos.size(); i++) {
+							cout << (i + 1) << ". " << "\nAceleracion: " << neumaticos[i]->aceleracion << endl;
+						}
+						cout << "Seleccione neumatico: ";
+						cin >> neumatico;
+						while (neumatico < 1 || neumatico > neumaticos.size()) {
+							cout << "Neumatico invalido!\nSeleccione neumatico: ";
+							cin >> neumatico;
+						}
+						neumatico--;
+						autos[pos]->aceleracion -= neumaticos[neumatico]->aceleracion;
+						cout << "\nResultados de modificacion\n";
+						cout << "Marca: " << autos[pos]->marca << "\nModelo: " << autos[pos]->modelo << "\nVelocidad Maxima: " << autos[pos]->velMax << "\nCaballos de fuerza: " << autos[pos]->caballosFuerza << "\nAceleracion: " << autos[pos]->aceleracion << endl;
 						system("pause");
 						break;
 					case 4:
 						system("cls");
-						cout << "Carrera\n";
+						cout << "--Modificar Diferencial--\n\n";
+						cout << "Diferenciales disponibles:\n";
+						for (int i = 0; i < diferenciales.size(); i++) {
+							cout << (i + 1) << ". " << "\nAceleracion: " << diferenciales[i]->aceleracion << endl;
+						}
+						cout << "Seleccione diferencial: ";
+						cin >> diferencial;
+						while (diferencial < 1 || diferencial > diferenciales.size()) {
+							cout << "Diferencial invalido!\nSeleccione diferencial: ";
+							cin >> diferencial;
+						}
+						diferencial--;
+						autos[pos]->aceleracion -= diferenciales[diferencial]->aceleracion;
+						cout << "\nResultados de modificacion\n";
+						cout << "Marca: " << autos[pos]->marca << "\nModelo: " << autos[pos]->modelo << "\nVelocidad Maxima: " << autos[pos]->velMax << "\nCaballos de fuerza: " << autos[pos]->caballosFuerza << "\nAceleracion: " << autos[pos]->aceleracion << endl;
 						system("pause");
 						break;
 					case 5:
+						cout << "--Modificar Caja de Cambios--\n\n";
+						cout << "Cajas de cambios disponibles:\n";
+						for (int i = 0; i < cajasDeCambios.size(); i++) {
+							cout << (i + 1) << ". " << "\nAceleracion: " << cajasDeCambios[i]->aceleracion << "\nVelocidad Maxima: " << cajasDeCambios[i]->velMax << endl;
+						}
+						cout << "Seleccione caja de cambios: ";
+						cin >> cajadc;
+						while (cajadc < 1 || cajadc > cajasDeCambios.size()) {
+							cout << "Caja de cambios invalida!\nSeleccione caja de cambios: ";
+							cin >> cajadc;
+						}
+						cajadc--;
+						autos[pos]->aceleracion -= cajasDeCambios[cajadc]->aceleracion;
+						autos[pos]->velMax += cajasDeCambios[cajadc]->velMax;
+						cout << "\nResultados de modificacion\n";
+						cout << "Marca: " << autos[pos]->marca << "\nModelo: " << autos[pos]->modelo << "\nVelocidad Maxima: " << autos[pos]->velMax << "\nCaballos de fuerza: " << autos[pos]->caballosFuerza << "\nAceleracion: " << autos[pos]->aceleracion << endl;
+						system("pause");
+						break;
+					default:
+						system("cls");
+						cout << "Opcion invalida!\n";
+						system("pause");
+						break;
+					}
+					break;
+					case 3:
+						system("cls");
+						cout << "--Ordenar Autos--\n";
+						cout << "Autos existentes:\n";
+						for (int i = 0; i < autos.size(); i++) {
+							cout << (i+1) << "\nMarca: " << autos[i]->marca << "\nModelo: " << autos[i]->modelo << "\nVelocidad Maxima: " << autos[i]->velMax << "\nCaballos de fuerza: " << autos[i]->caballosFuerza << "\nAceleracion: " << autos[i]->aceleracion << endl;
+						}
+						cout << "\nEspecificaciones de los autos:\n1. Velocidad Maxima\n2. Caballos de fuerza\n3. Aceleracion\nSeleccione aspecto para ordenar:\n";
+						cin >> ordenar;
+						while (ordenar < 1 || ordenar > 3) {
+							cout << "Opcion invalida!\nSeleccione aspecto para ordenar:\n";
+							cin >> ordenar;
+						}
+						system("cls");
+						cout << "Resultados del ordenamiento\n";
+						switch (ordenar) {
+						case 1:
+							//Ordenado del vector de carros en base a velocidad maxima
+							for (int i = 0; i < autos.size(); i++) {
+								for (int j = 0; j < autos.size() - 1; j++) {
+									if (autos[j]->velMax < autos[j + 1]->velMax) {
+										Auto* temp = autos[j];
+										autos[j] = autos[j + 1];
+										autos[j + 1] = temp;
+									}
+								}
+							}
+							for (int i = 0; i < autos.size(); i++) {
+								cout << (i + 1) << "\nMarca: " << autos[i]->marca << "\nModelo: " << autos[i]->modelo << "\nVelocidad Maxima: " << autos[i]->velMax << "\nCaballos de fuerza: " << autos[i]->caballosFuerza << "\nAceleracion: " << autos[i]->aceleracion << endl;
+							}
+							break;
+						case 2:
+							//Ordenado del vector de carros en base a caballos de fuerza
+							for (int i = 0; i < autos.size(); i++) {
+								for (int j = 0; j < autos.size() - 1; j++) {
+									if (autos[j]->caballosFuerza < autos[j + 1]->caballosFuerza) {
+										Auto* temp = autos[j];
+										autos[j] = autos[j + 1];
+										autos[j + 1] = temp;
+									}
+								}
+							}
+							for (int i = 0; i < autos.size(); i++) {
+								cout << (i + 1) << "\nMarca: " << autos[i]->marca << "\nModelo: " << autos[i]->modelo << "\nVelocidad Maxima: " << autos[i]->velMax << "\nCaballos de fuerza: " << autos[i]->caballosFuerza << "\nAceleracion: " << autos[i]->aceleracion << endl;
+							}
+							break;
+						case 3:
+							//Ordenado del vector de carros en base a aceleracion
+							for (int i = 0; i < autos.size(); i++) {
+								for (int j = 0; j < autos.size() - 1; j++) {
+									if (autos[j]->aceleracion < autos[j + 1]->aceleracion) {
+										Auto* temp = autos[j];
+										autos[j] = autos[j + 1];
+										autos[j + 1] = temp;
+									}
+								}
+							}
+							for (int i = 0; i < autos.size(); i++) {
+								cout << (i + 1) << "\nMarca: " << autos[i]->marca << "\nModelo: " << autos[i]->modelo << "\nVelocidad Maxima: " << autos[i]->velMax << "\nCaballos de fuerza: " << autos[i]->caballosFuerza << "\nAceleracion: " << autos[i]->aceleracion << endl;
+							}
+							break;
+						}
+						system("pause");
+						break;
+					case 4:
+						system("cls");
+						cout << "--Carrera--\n";
+						cout << "Autos existentes:\n";
+						for (int i = 0; i < autos.size(); i++) {
+							cout << (i + 1) << "\nMarca: " << autos[i]->marca << "\nModelo: " << autos[i]->modelo << "\nVelocidad Maxima: " << autos[i]->velMax << "\nCaballos de fuerza: " << autos[i]->caballosFuerza << "\nAceleracion: " << autos[i]->aceleracion << endl;
+						}
+						cout << "Seleccione el auto 1: ";
+						cin >> auto1;
+						cout << "Seleccione el auto 2: ";
+						cin >> auto2;
+						cout << "Ingrese la distancia de la carrera (en km): ";
+						cin >> distancia;
+						auto1--;
+						auto2--;
+						//se les resta 1 a las variables ya que se ingresa el valor que se ve en pantalla pero en realidad es ese valor -1.
+						carrera(autos[auto1]->velMax, autos[auto2]->velMax, autos[auto1]->aceleracion, autos[auto2]->aceleracion, distancia);
+						system("cls");
+						break;
+					case 5:
+						nfs = false;
 						system("cls");
 						break;
 					default:
